aa19ccc6c83741bea04b6846cc8b27582bff65a7
Hamza Sar <hamoz.sq78@gmail.com>
2020-09-17 00:26:11 -0400

    added 1.1 and general tweaks

---
+++ global_assets/js/assignment.js
@@ -0,0 +1,14 @@
+const content = document.querySelector('.assignment-content');
+const contentLength = document.querySelector('.assignment-content').children.length;
+
+var questionsListed = 0;
+
+function questionDrawIn() {
+	content.children[questionsListed].classList.add('question-draw-in');
+	questionsListed++;
+	if (questionsListed < contentLength) setTimeout(questionDrawIn, 400);
+}
+
+setTimeout(function() {
+	if (questionsListed < contentLength) questionDrawIn();
+}, 1500);

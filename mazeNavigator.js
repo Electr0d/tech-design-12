7c0bcfd68db155182edd6eee77193a6ef73a46dc
Hamza Sar <hamoz.sq78@gmail.com>
2020-10-26 13:58:19 -0400

    added maze study

--- mazeNavigator.js
+++ mazeNavigator.js
@@ -37,39 +37,59 @@ const mazes = {
 			type: 'roll',
 			speed: 50,
 			direction: -35,
-			duration: 1.35
+			duration: 1.3
 		},
-		
+
 		// phase 4
 		step_5: {
 			type: 'stop'
 		},
-		
+
 		step_6: {
 			type: 'roll',
-			speed: 76,
+			speed: 43,
 			direction: 60,
-			duration: 1.4
+			duration: 2
 		},
-		
+
 		// phase 5
 		step_7: {
-			type: 'delay',
+			type: 'stop',
 			duration: 2,
 		},
 		step_8: {
-			type: 'roll',
-			speed: 150,
+			type: 'set',
 			direction: -30,
-			duration: 1
 		},
+
 		step_9: {
 			type: 'roll',
 			speed: 150,
-			direction: -40,
-			duration: 1
+			direction: -30,
+			duration: 0.93
 		},
-		length: 9
+		step_10: {
+		type: 'stop'
+		},
+		
+		step_11: {
+			type: 'roll',
+			speed: 165,
+			direction: -45,
+			duration: 0.64,
+		},
+		step_12: {
+		type: 'stop'
+		},
+
+		// phase 6
+		step_13: {
+			type: 'roll',
+			speed: 100,
+			direction: 0,
+			duration: 0.5,
+		},
+		length: 14
 		// index: 0
 	}
 };
@@ -98,6 +118,37 @@ function invertColor() {
 		rgb.effects.invert = false;
 	}
 }
+async function maze(maze, direction) {
+	let mazeLength = mazes[maze].length;
+
+	if (direction == 'normal') {
+		// maze code
+
+		// executeStep(mazes[maze]);
+		
+		for (let i = 0; i < mazeLength; i++) {
+			let step = mazes[maze]['step_' + i];
+			
+			if (step.type == 'set') {
+				led(255, 80, 20);
+				setHeading(step.direction);
+			} else if (step.type == 'roll') {
+				led(50, 200, 50);
+				await roll(step.direction, step.speed, step.duration);
+			} else if (step.type == 'stop') {
+				led(200, 25, 25);
+				stopRoll();
+			}
+		}
+	}
+}
+async function onCollision() {
+	await speak('OW!');
+}
+
+
+
+
 
 function epilepsyMode(duration) {
 	if (!rgb.effects.epilepsyMode) {
@@ -113,33 +164,4 @@ function epilepsyMode(duration) {
 	} else {
 		rgb.effects.epilepsyMode = false;
 	}
-}
-async function maze(maze, direction) {
-	let mazeLength = mazes[maze].length;
-
-	if (direction == 'normal') {
-		// maze code
-
-		// executeStep(mazes[maze]);
-
-		for (let i = 0; i < mazeLength; i++) {
-			let step = mazes[maze]['step_' + i];
-
-			if (step.type == 'set') {
-				led(255, 80, 20);
-				setHeading(step.direction);
-			} else if (step.type == 'roll') {
-				led(50, 200, 50);
-				await roll(step.direction, step.speed, step.duration);
-			} else if (step.type == 'stop') {
-				led(200, 25, 25);
-				stopRoll();
-			} else if (step.type == 'delay') {
-				await delay(Number(type.duration));
-			}
-		}
-	}
-}
-async function onCollision() {
-	await speak('OW!');
 }
